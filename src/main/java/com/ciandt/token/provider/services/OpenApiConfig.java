c92521d4f8028f8e8f2a59e05de5bb271616c261
amostaro <amostaro@ciandt.com>
2022-09-05 14:39:49 -0300

    ajuste pagina swagger

--- src/main/java/com/ciandt/token/provider/services/OpenApiConfig.java
+++ src/main/java/com/ciandt/token/provider/services/OpenApiConfig.java
@@ -19,7 +19,7 @@ import java.io.IOException;
 @RestController
 public class OpenApiConfig {
 
-    @GetMapping(value = "/swagger-ui/index.html")
+    @GetMapping(value = "/")
     public void index(HttpServletResponse response) throws IOException {
         response.sendRedirect("/swagger-ui/index.html");
     }
